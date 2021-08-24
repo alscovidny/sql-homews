a78503d02eea15e3a0bb36e30bf69400f633d98e
Alexander <sagaradze96@gmail.com>
2021-08-24 13:23:03 +0300

    скопировали в папку с репозиторием

---
+++ als_my_vk.sql
@@ -0,0 +1,108 @@
+DROP DATABASE IF EXISTS vk;
+CREATE DATABASE vk;
+USE vk;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+	id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	firstname VARCHAR(100),
+	lastname VARCHAR(100),
+	email VARCHAR(200) UNIQUE,
+	password VARCHAR(255),
+	phone BIGINT UNSIGNED UNIQUE,
+	INDEX idx_users_username(firstname, lastname)
+);
+
+DROP TABLE IF EXISTS profiles;
+CREATE TABLE profiles(
+	user_id BIGINT UNSIGNED NOT NULL PRIMARY KEY,
+	gender VARCHAR(1),
+	hometown VARCHAR(255),
+	created_at DATETIME DEFAULT NOW()
+);
+
+ALTER TABLE profiles ADD CONSTRAINT fk_profiles_user_id
+FOREIGN KEY (user_id) REFERENCES users(id);
+
+#один ко многим 1 x M
+DROP TABLE IF EXISTS messages;
+CREATE TABLE messages(
+	id SERIAL,
+	from_user_id BIGINT UNSIGNED NOT NULL,
+	to_user_id BIGINT UNSIGNED NOT NULL,
+	body TEXT,
+	created_at DATETIME DEFAULT NOW(),
+	FOREIGN KEY (from_user_id) REFERENCES users(id),
+	FOREIGN KEY (to_user_id) REFERENCES users(id)
+);
+
+DROP TABLE IF EXISTS friend_requests;
+CREATE TABLE friend_requests(
+	initiator_user_id BIGINT UNSIGNED NOT NULL,
+	target_user_id BIGINT UNSIGNED NOT NULL,
+	status ENUM('requested', 'approved', 'declined', 'unfriended'),
+	
+	created_at DATETIME DEFAULT NOW(),
+	updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP,
+	
+	PRIMARY KEY (initiator_user_id, target_user_id),
+	FOREIGN KEY (initiator_user_id) REFERENCES users(id),
+	FOREIGN KEY (target_user_id) REFERENCES users(id),
+	
+	CHECK (initiator_user_id != target_user_id)
+);
+
+DROP TABLE IF EXISTS communities;
+CREATE TABLE communities(
+	id SERIAL,
+	name VARCHAR(255),
+	admin_user_id BIGINT UNSIGNED NOT NULL,
+	
+	INDEX(name),
+	FOREIGN KEY (admin_user_id) REFERENCES users(id)
+);
+
+DROP TABLE IF EXISTS users_communities;
+CREATE TABLE users_communities(
+	user_id BIGINT UNSIGNED NOT NULL,
+	community_id BIGINT UNSIGNED NOT NULL,
+	
+	FOREIGN KEY (user_id) REFERENCES users(id),
+	FOREIGN KEY (community_id) REFERENCES communities(id)
+);
+
+DROP TABLE IF EXISTS media_types;
+CREATE TABLE media_types(
+	id SERIAL,
+	name VARCHAR(255)
+
+);
+
+DROP TABLE IF EXISTS media;
+CREATE TABLE media(
+	id SERIAL,
+	user_id BIGINT UNSIGNED NOT NULL,
+	media_type_id BIGINT UNSIGNED NOT NULL,
+	body VARCHAR(255),
+	#file BLOB,
+	filename VARCHAR(255),
+	metadata JSON,
+	created_at DATETIME DEFAULT NOW(),
+	updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP,
+	
+	FOREIGN KEY (user_id) REFERENCES users(id),
+	FOREIGN KEY (media_type_id) REFERENCES media_types(id)
+	
+);
+
+DROP TABLE IF EXISTS likes;
+CREATE TABLE likes(
+	id SERIAL,
+	user_id BIGINT UNSIGNED NOT NULL,
+	media_id BIGINT UNSIGNED NOT NULL
+	
+	
+);
+
+ALTER TABLE vk.likes ADD CONSTRAINT likes_FK FOREIGN KEY (media_id) REFERENCES vk.media(id);
+ALTER TABLE vk.likes ADD CONSTRAINT likes_FK_1 FOREIGN KEY (user_id) REFERENCES vk.users(id);
